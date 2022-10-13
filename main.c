ee4837c6252fb6b7c9144ac04511e766943961a9
aya <yifanlin@mail.ustc.edu.cn>
2022-10-13 10:21:56 +0800

    modify main.c

--- main.c
+++ main.c
@@ -2,66 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 #include <malloc.h>
+#include "zeros.h"
+#include "sort.h"
 
-#define IP_ADDR unsigned int
+
 #define SEED 7878696
 
-// a and b are random coefficients of hash function
-IP_ADDR a;
-IP_ADDR b;
 
-IP_ADDR hash(IP_ADDR ip_addr)
-{
-    // return the hash value of ip_addr
-    long long int value;
-    value = (ip_addr * a + b) % (long long int)(pow(2, 32));
-    return (IP_ADDR)value;
-}
 
-int zeros(IP_ADDR hash_value)
-{
-    // return the number of rightmost zeros of a hash_value
-    int zeros = 0;
-    int flag;
-    do
-    {
-        flag = (hash_value >> zeros) & 1;
-        zeros++;
-    } while (flag == 0);
-    return zeros;
-}
 
-void distinct_elements(IP_ADDR ip_addr, int *max_zeros)
-{
-    // update the value of max_zeros
-    int z = zeros(hash(ip_addr));
-    if (zeros(hash(ip_addr)) > *max_zeros)
-        *max_zeros = z;
-    return;
-}
 
-void swap(int *p, int *q)
-{
-    int t;
 
-    t = *p;
-    *p = *q;
-    *q = t;
-}
 
-void sort(int a[], int n)
-{
-    int i, j, temp;
 
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = 0; j < n - i - 1; j++)
-        {
-            if (a[j] > a[j + 1])
-                swap(&a[j], &a[j + 1]);
-        }
-    }
-}
+
+
 
 int main()
 {
@@ -70,6 +25,9 @@ int main()
     int max_zeros = 0;
     int ntimes, i;
     int a1, a2, a3, a4, b1, b2, b3, b4;
+
+    // a and b are random coefficients of hash function
+    IP_ADDR a, b;
 
     int *max_zeros_arr;
 
@@ -103,7 +61,7 @@ int main()
         while ((fscanf(fp, "%d.%d.%d.%d", &ip_addr_1, &ip_addr_2, &ip_addr_3, &ip_addr_4)) != EOF)
         {
             ip_addr = ip_addr_1 * pow(2, 24) + ip_addr_2 * pow(2, 16) + ip_addr_3 * pow(2, 8) + ip_addr_4;
-            distinct_elements(ip_addr, &max_zeros);
+            distinct_elements(ip_addr, &max_zeros, a, b);
         }
         max_zeros_arr[i] = max_zeros;
         fclose(fp);
